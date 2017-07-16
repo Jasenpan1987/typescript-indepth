109265139e3e34229dd82b78ddabef443b0a2aab
Jasen Pan <jasenpan@gmail.com>
2017-07-16 14:20:53 +1000

    add enums

--- app.ts
+++ app.ts
@@ -1,8 +1,46 @@
-class HelloWorld {
-    constructor (public message: string){
+function getAllBooks() {
+    let books = [
+        { title:"Node JS Guide", author: "justjavac", available: "true", category: Category.Biography },
+        { title: "Introduction to react", author: "facebook", available: "true", category: Category.Fiction },
+        { title: "How to use firebase", author: "google", available: false, category: Category.Poetry }
+    ];
 
-    }
+    return books;
 }
 
-var hello = new HelloWorld("Hello TS");
-console.log(hello.message);
+function logFirstAvailable (books):void {
+    let numberOfBooks = books.length;
+    let bookAvailable: string = "";
+
+    for (let book of books) {
+        if(book.available) {
+            bookAvailable = book.title;
+            break;
+        }
+    }
+
+    console.log("Total number of books: " + numberOfBooks);
+    console.log("First book available: " + bookAvailable);
+}
+
+enum Category {
+    Biography, Poetry, Fiction, History, Children
+};
+
+function getBookTitlesByCategory (category: Category): Array<string> {
+    console.log(`Getting books with category ${category} (${Category[category]})`);
+    const allBooks = getAllBooks();
+    return allBooks.filter(book => book.category === category).map(book => book.title);
+}
+
+function logBookTitles (titleArr: string[]): void {
+    titleArr.forEach(title => console.log(`Title: ${title}`));
+}
+
+const allbooks = getAllBooks();
+logFirstAvailable(allbooks);
+
+const allFictionBookTitles = getBookTitlesByCategory(Category.Fiction);
+logBookTitles(allFictionBookTitles);
+
+
--- js/app.js
+++ js/app.js
@@ -1,9 +1,43 @@
-var HelloWorld = (function () {
-    function HelloWorld(message) {
-        this.message = message;
+function getAllBooks() {
+    var books = [
+        { title: "Node JS Guide", author: "justjavac", available: "true", category: Category.Biography },
+        { title: "Introduction to react", author: "facebook", available: "true", category: Category.Fiction },
+        { title: "How to use firebase", author: "google", available: false, category: Category.Poetry }
+    ];
+    return books;
+}
+function logFirstAvailable(books) {
+    var numberOfBooks = books.length;
+    var bookAvailable = "";
+    for (var _i = 0, books_1 = books; _i < books_1.length; _i++) {
+        var book = books_1[_i];
+        if (book.available) {
+            bookAvailable = book.title;
+            break;
+        }
     }
-    return HelloWorld;
-}());
-var hello = new HelloWorld("Hello TS");
-console.log(hello.message);
+    console.log("Total number of books: " + numberOfBooks);
+    console.log("First book available: " + bookAvailable);
+}
+var Category;
+(function (Category) {
+    Category[Category["Biography"] = 0] = "Biography";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+    Category[Category["History"] = 3] = "History";
+    Category[Category["Children"] = 4] = "Children";
+})(Category || (Category = {}));
+;
+function getBookTitlesByCategory(category) {
+    console.log("Getting books with category " + category + " (" + Category[category] + ")");
+    var allBooks = getAllBooks();
+    return allBooks.filter(function (book) { return book.category === category; }).map(function (book) { return book.title; });
+}
+function logBookTitles(titleArr) {
+    titleArr.forEach(function (title) { return console.log("Title: " + title); });
+}
+var allbooks = getAllBooks();
+logFirstAvailable(allbooks);
+var allFictionBookTitles = getBookTitlesByCategory(Category.Fiction);
+logBookTitles(allFictionBookTitles);
 //# sourceMappingURL=app.js.map
